bbc9e7b842aabd29774206d1e24ecc1dd10c1c31
amagana10 <32153064+amagana10@users.noreply.github.com>
2020-12-06 17:51:52 -0600

    Update AccountHolderNotFoundException.java

--- src/main/java/com/meritamerica/assignment5/AccountHolderNotFoundException.java
+++ src/main/java/com/meritamerica/assignment5/AccountHolderNotFoundException.java
@@ -1,6 +1,7 @@
 package com.meritamerica.assignment5;
 class AccountHolderNotFoundException extends RuntimeException {
 
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
   AccountHolderNotFoundException(Long id) {
     super("Could not find employee " + id);
   }
