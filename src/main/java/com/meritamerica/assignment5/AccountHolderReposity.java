677975bd531e0c71240f93451d9cf7af8ddc737d
Jameson <jamesonsquyres@gmail.com>
2020-12-13 23:03:22 -0600

    removed repo

--- src/main/java/com/meritamerica/assignment5/AccountHolderReposity.java
+++
@@ -1,9 +0,0 @@
-package com.meritamerica.assignment5;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountHolderReposity extends JpaRepository<AccountHolder, Integer>{
-	
-	AccountHolder findById(int id);
-
-}
