02f140b9fe57a1ab7879f98d8429edfbb3480a1d
Jameson <jamesonsquyres@gmail.com>
2021-01-02 22:02:56 -0600

    added user details, needs fix

--- src/main/java/com/meritamerica/assignment7/controller/MeritBankController.java
+++ src/main/java/com/meritamerica/assignment7/controller/MeritBankController.java
@@ -6,6 +6,11 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +23,8 @@ import com.meritamerica.assignment7.exceptions.AccountHolderNotFoundException;
 import com.meritamerica.assignment7.exceptions.ExceedsCombinedBalanceLimitException;
 import com.meritamerica.assignment7.models.AccountHolder;
 import com.meritamerica.assignment7.models.AccountHolderContactDetails;
+import com.meritamerica.assignment7.models.AuthenticationRequest;
+import com.meritamerica.assignment7.models.AuthenticationResponse;
 import com.meritamerica.assignment7.models.CDAccount;
 import com.meritamerica.assignment7.models.CDOffering;
 import com.meritamerica.assignment7.models.CheckingAccount;
@@ -28,6 +35,8 @@ import com.meritamerica.assignment7.repositories.CDAccountRepository;
 import com.meritamerica.assignment7.repositories.CDOfferingRepository;
 import com.meritamerica.assignment7.repositories.CheckingAccountRepository;
 import com.meritamerica.assignment7.repositories.SavingsAccountRepository;
+import com.meritamerica.assignment7.services.JwtUtil;
+import com.meritamerica.assignment7.services.MyUserDetailsService;
 
 @RestController
 public class MeritBankController {
@@ -44,6 +53,32 @@ public class MeritBankController {
 	private CheckingAccountRepository checkingAccountRepository;
 	@Autowired
 	private SavingsAccountRepository savingsAccountRepository;
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Autowired
+	private JwtUtil jwtTokenUtil;
+
+	@Autowired
+	private MyUserDetailsService userDetailsService;
+
+	@PostMapping("/authenticate")
+	public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest)
+			throws Exception {
+
+		try {
+			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+					authenticationRequest.getUsername(), authenticationRequest.getPassword()));
+		} catch (BadCredentialsException e) {
+			throw new Exception("Incorrect username or password", e);
+		}
+
+		final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+
+		final String jwt = jwtTokenUtil.generateToken(userDetails);
+
+		return ResponseEntity.ok(new AuthenticationResponse(jwt));
+	}
 
 	@PostMapping("/AccountHolders")
 	@ResponseStatus(HttpStatus.CREATED)
